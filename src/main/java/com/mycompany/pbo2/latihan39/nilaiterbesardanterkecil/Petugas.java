bcd2e602467d0ef3cc9240a83de998e45e8ecfc4
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-10-17 17:33:19 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author asus
+ * Nama     : Muhamad Rizqi Ramdani
+ * Kelas    : PBO2
+ * Nim      : 10118069
+ * Deskripsi Tugas : Nilai Terkecil dan Terbesar
+ */
+public class Petugas {
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
